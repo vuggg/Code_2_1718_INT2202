229a1dc5b816a4c3cf45969678ddebbed22ab442
vuggg <vuongpyte@gmail.com>
2018-03-27 11:36:48 +0700

    chekc hai mảng bằng nhau

---
+++ Week_5/hai_mang_bang_nhau.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool eq( int *ar1, int *ar2, int n)
+{
+    sort(ar1,ar1+n);
+    sort(ar2,ar2+ n);
+    for(int i=0;i<n;i++)
+    {
+        if(ar1[i]!=ar2[i]) return false;
+        return true;
+    }
+}
+
+int main() {
+    int n; cin>>n;
+    int arr1[n], arr2[n];
+    for(int i = 0; i < n; i++) {
+        cin>>arr1[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cin>>arr2[i];
+    }
+
+    bool result = eq(arr1, arr2, n);
+    if(result) {
+        cout<<"true";
+    } else {
+        cout<<"false";
+    }
+    return 0;
+}
