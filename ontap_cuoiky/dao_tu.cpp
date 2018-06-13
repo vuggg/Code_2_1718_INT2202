f66be06d7f49f629d766a42bca844f13b2e2a7ca
vuggg <vuongpyte@gmail.com>
2018-06-13 14:57:43 +0700

    đảo từ trong câu

--- ontap_cuoiky/dao_tu.cpp
+++ ontap_cuoiky/dao_tu.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+bool check(string &s, int idx)
+{
+    if( (idx+1)<s.size() )
+    {
+        if(s.at(idx) != ' ') return true;
+    }
+    return false;
+}
+void stringToVector(string &s, vector<string> &listWord, int &vectorSize)
+{
+    /// gan dai hoc
+    for(int i=0;i< s.size();i++)
+    {
+        int idx = i;
+        while( check(s,idx) ) idx++;
+        if(idx == s.size() -1) idx++;
+        listWord.at(vectorSize) = s.substr(i,idx-i);
+        //cout << listWord.at(vectorSize) << endl;
+        i = idx;
+        vectorSize++;
+    }
+}
+
+int main()
+{
+    vector<string> listWord(100);
+    string s;
+    int vectorSize = 0;
+    getline(cin,s);
+    stringToVector(s,listWord,vectorSize);
+    for(int i = vectorSize-1;i>=0;i--)
+    {
+        cout << listWord.at(i) << " ";
+    }
+}
